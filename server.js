26c02cf7bc7951d39e59e5c21c12ae21c5f5ab65
root <AleeArmaghan>
2021-12-29 14:39:07 +0000

    Docker build fo app

--- server.js
+++ server.js
@@ -9,10 +9,9 @@ var handleRequest = function(request, response) {
  response.write("DevOps Coursework 2! | Running on: ");
  response.write(host);
  response.end(" | v=0\n");
- console.log("Running On:" ,host, "| Total Requests:", ++requests,"| App
-Uptime:", (new Date() - startTime)/1000 , "seconds", "| Log Time:",new Date());}
+ console.log("Running On:" ,host, "| Total Requests:", ++requests,"| App Uptime:", (new Date() - startTime)/1000 , "seconds", "| Log Time:",new Date());}
 var www = http.createServer(handleRequest);
-www.listen(8080,function () {
+www.listen(8000,function () {
  startTime = new Date();;
  host = process.env.HOSTNAME;
  console.log ("Started At:",startTime, "| Running On: " ,host, "\n" );
